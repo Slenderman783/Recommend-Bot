94dd9100a4338324770f66f414b936b89dbe6d08
Slenderman783 <89049133+Slenderman783@users.noreply.github.com>
2023-12-28 20:00:08 +0000

    Add files via upload

--- bot.js
+++ bot.js
@@ -3,9 +3,9 @@ const PlexAPI = require('plex-api');
 const fetch = require('isomorphic-fetch');
 const TMDB_API_KEY = '847803d90ece3f3243d414fa7586b1da';
 
-const discordToken = 'MTEwODE0MjEwMzUzNzY2NDEzMw.G0pGP3.LO_kBtb-hHi29gPebCIJJb8lD2_W_iL7qaFbuA';
+const discordToken = 'MTEwODE0MjEwMzUzNzY2NDEzMw.G4K_sQ.Wl-2ZXQiH72ATQ3M6AGtE49AkvxIYIa-_IXrz';
 const plexConfig = {
-  hostname: '192.168.1.99',
+  hostname: '192.168.0.99',
   port: 32400,
   username: 'Slenderman783',
   password: 'Gorhamm001',
@@ -13,7 +13,6 @@ const plexConfig = {
 
 let prefix = '!'; // Default command prefix
 const favoriteShows = {}; // Object to store favorite shows for each user
-const blacklist = []; // Array to store blacklisted item keys
 
 const client = new Discord.Client();
 
@@ -64,175 +63,196 @@ client.on('message', async (message) => {
       // Retrieve a random section key
       const randomSectionKey = matchingSectionKeys[Math.floor(Math.random() * matchingSectionKeys.length)];
 
-      // Retrieve a random item from the chosen section, excluding those in the blacklist
+      // Retrieve a random item from the chosen section
       const items = await plex.query(`/library/sections/${randomSectionKey}/all?X-Plex-Container-Start=0&X-Plex-Container-Size=50`);
-      const filteredItems = applyFilters(items.MediaContainer.Metadata, args);
 
-      if (filteredItems.length === 0) {
-        message.channel.send('No results found matching the specified filters.');
+      if (items.MediaContainer.Metadata.length === 0) {
+        message.channel.send(`There are no ${mediaType}s available in the selected section.`);
         return;
       }
 
-      const randomItem = filteredItems[Math.floor(Math.random() * filteredItems.length)];
-
-      // Fetch additional details and ratings from TMDb API
-      let tmdbData = null;
-      if (mediaType === 'movie') {
-        tmdbData = await fetchMovieDetails(randomItem.title, randomItem.year);
-      } else {
-        tmdbData = await fetchTVShowDetails(randomItem.title);
+      // Generate the specified number of recommendations
+      const numRecommendations = parseInt(args[0]);
+      if (isNaN(numRecommendations) || numRecommendations <= 0) {
+        message.channel.send('Please provide a valid number of recommendations.');
+        return;
       }
 
-      const recommendationEmbed = createRecommendationEmbed(randomItem, tmdbData, mediaType);
+      const recommendations = [];
+      for (let i = 0; i < numRecommendations; i++) {
+        const randomItem = items.MediaContainer.Metadata[Math.floor(Math.random() * items.MediaContainer.Metadata.length)];
+
+        // Fetch additional details and ratings from TMDb API
+        let tmdbData = null;
+        try {
+          tmdbData = await fetchTMDBData(randomItem.title, mediaType);
+        } catch (error) {
+          console.error('An error occurred while fetching data from TMDb:', error.message);
+        }
+
+        if (!tmdbData) {
+          message.channel.send(`Failed to fetch additional details for ${randomItem.title}`);
+          continue;
+        }
+
+        const recommendation = {
+          title: randomItem.title,
+          summary: randomItem.summary,
+          rating: tmdbData.rating,
+          year: tmdbData.year,
+          genres: tmdbData.genres.join(', '),
+          poster: tmdbData.poster,
+        };
+
+        recommendations.push(recommendation);
+      }
+
+      if (recommendations.length === 0) {
+        message.channel.send('No recommendations available.');
+        return;
+      }
+
+      // Send the list of recommendations
+      const recommendationEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(`${numRecommendations} Recommendations`)
+        .setDescription(`Here are ${numRecommendations} ${mediaType}s for you:`);
+
+      recommendations.forEach((recommendation) => {
+        recommendationEmbed.addField(recommendation.title, `Rating: ${recommendation.rating}, Year: ${recommendation.year}`);
+      });
+
       message.channel.send(recommendationEmbed);
     } catch (error) {
-      console.error(error);
-      message.channel.send('An error occurred while retrieving recommendations. Please try again later.');
+      console.error('An error occurred:', error.message);
+      message.channel.send('An error occurred while processing your request. Please try again later.');
     }
-  } else if (command === 'favorite') {
-    const showName = args.join(' ').toLowerCase();
+  } else if (command === 'prefix') {
+    if (!args[0]) {
+      message.channel.send(`The current prefix is \`${prefix}\``);
+    } else {
+      prefix = args[0];
+      message.channel.send(`Prefix set to \`${prefix}\``);
+    }
+  } else if (command === 'addfavourite') {
     const userId = message.author.id;
-
-    if (!showName) {
-      message.channel.send('Please provide a show name to add to your favorites.');
-      return;
-    }
+    const showTitle = args.join(' ').toLowerCase();
 
     if (!favoriteShows[userId]) {
       favoriteShows[userId] = [];
     }
 
-    if (favoriteShows[userId].includes(showName)) {
-      message.channel.send('This show is already in your favorites.');
+    if (favoriteShows[userId].includes(showTitle)) {
+      message.channel.send(`You have already added ${showTitle} to your favorite shows.`);
     } else {
-      favoriteShows[userId].push(showName);
-      message.channel.send(`The show "${showName}" has been added to your favorites.`);
+      favoriteShows[userId].push(showTitle);
+      message.channel.send(`Successfully added ${showTitle} to your favorite shows.`);
     }
-  } else if (command === 'favorites') {
+  } else if (command === 'removefavourite') {
     const userId = message.author.id;
-    const userFavorites = favoriteShows[userId];
-
-    if (!userFavorites || userFavorites.length === 0) {
-      message.channel.send('You have no favorite shows yet.');
-      return;
-    }
-
-    const favoritesList = userFavorites.map((show) => `- ${show}`).join('\n');
-    message.channel.send(`Your favorite shows:\n${favoritesList}`);
-  } else if (command === 'remove') {
-    const showName = args.join(' ').toLowerCase();
-    const userId = message.author.id;
-
-    if (!showName) {
-      message.channel.send('Please provide a show name to remove from your favorites.');
-      return;
-    }
+    const showTitle = args.join(' ').toLowerCase();
 
     if (!favoriteShows[userId] || favoriteShows[userId].length === 0) {
       message.channel.send('You have no favorite shows to remove.');
-      return;
-    }
-
-    const index = favoriteShows[userId].indexOf(showName);
-    if (index !== -1) {
-      favoriteShows[userId].splice(index, 1);
-      message.channel.send(`The show "${showName}" has been removed from your favorites.`);
     } else {
-      message.channel.send('This show is not in your favorites.');
-    }
-  } else if (command === 'blacklist') {
-    if (!args.length) {
-      message.channel.send('You must provide an item key to blacklist.');
-      return;
-    }
-    const itemKey = args[0];
+      const index = favoriteShows[userId].findIndex((title) => title === showTitle);
 
-    // Add the item key to the blacklist
-    blacklist.push(itemKey);
+      if (index === -1) {
+        message.channel.send(`${showTitle} is not in your favorite shows list.`);
+      } else {
+        favoriteShows[userId].splice(index, 1);
+        message.channel.send(`Successfully removed ${showTitle} from your favorite shows.`);
+      }
+    }
+  } else if (command === 'favorites') {
+    const userId = message.author.id;
 
-    message.channel.send(`Item with key ${itemKey} has been added to the blacklist.`);
+    if (!favoriteShows[userId] || favoriteShows[userId].length === 0) {
+      message.channel.send('You have no favorite shows.');
+    } else {
+      const favoriteShowsList = favoriteShows[userId].join(', ');
+      message.channel.send(`Your favorite shows: ${favoriteShowsList}`);
+    }
+  } else if (command === 'searchgenre') {
+    const genre = args.join(' ').toLowerCase();
+
+    try {
+      const tmdbData = await searchTMDBByGenre(genre);
+
+      if (tmdbData.length === 0) {
+        message.channel.send(`No results found for the genre: ${genre}`);
+        return;
+      }
+
+      const genreEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(`Search Results for Genre: ${genre}`);
+
+      tmdbData.forEach((item) => {
+        genreEmbed.addField(item.title, `Rating: ${item.rating}, Year: ${item.year}`);
+      });
+
+      message.channel.send(genreEmbed);
+    } catch (error) {
+      console.error('An error occurred while searching for genre:', error.message);
+      message.channel.send('An error occurred while searching for the genre. Please try again later.');
+    }
+  } else if (command === 'help') {
+    const helpEmbed = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('Command List')
+      .addField('!recommend [movie|tv show]', 'Get a random movie or TV show recommendation')
+      .addField('!recommendations [num]', 'Get multiple random movie or TV show recommendations')
+      .addField('!prefix [newPrefix]', 'Set a new command prefix')
+      .addField('!addfavourite [showTitle]', 'Add a TV show to your favorite shows')
+      .addField('!removefavourite [showTitle]', 'Remove a TV show from your favorite shows')
+      .addField('!favorites', 'List all your favorite shows')
+      .addField('!searchgenre [genre]', 'Search for movies and TV shows by genre')
+      .addField('!help', 'Show the list of commands and how to use them');
+
+    message.channel.send(helpEmbed);
   }
 });
 
-// Function to apply filters and exclude items from the blacklist
-function applyFilters(items, filters) {
-  return items.filter((item) => {
-    console.log('Item:', item); // Log the item to inspect its structure
-
-    // Ensure that the genre information is stored in the "Genre" field
-    const genre = item.Genre ? item.Genre.toLowerCase() : '';
-
-    // Exclude items from the blacklist
-    if (blacklist.includes(item.key)) {
-      return false;
-    }
-
-    // Exclude items from the "Porn" library
-    if (item.library.toLowerCase() === 'porn') {
-      return false;
-    }
-
-    for (const filter of filters) {
-      if (!matchesFilter(item, filter, genre)) {
-        return false;
-      }
-    }
-    return true;
-  });
-}
-
-// Function to fetch movie details from TMDb API
-async function fetchMovieDetails(title, year) {
+async function fetchTMDBData(title, mediaType) {
   const encodedTitle = encodeURIComponent(title);
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${encodedTitle}&year=${year}`;
+  const url = `https://api.themoviedb.org/3/search/${mediaType}?api_key=${TMDB_API_KEY}&query=${encodedTitle}`;
+
   const response = await fetch(url);
   const data = await response.json();
-  return data.results.length > 0 ? data.results[0] : null;
-}
 
-// Function to fetch TV show details from TMDb API
-async function fetchTVShowDetails(title) {
-  const encodedTitle = encodeURIComponent(title);
-  const url = `https://api.themoviedb.org/3/search/tv?api_key=${TMDB_API_KEY}&query=${encodedTitle}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data.results.length > 0 ? data.results[0] : null;
-}
-
-// Function to create an embedded message for the recommendation
-function createRecommendationEmbed(item, tmdbData, mediaType) {
-  const embed = new Discord.MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle(item.title)
-    .setDescription(item.summary)
-    .setThumbnail(item.thumb);
-
-  if (tmdbData) {
-    if (mediaType === 'movie') {
-      const releaseDate = tmdbData.release_date ? new Date(tmdbData.release_date).toLocaleDateString() : 'Unknown';
-      embed.addField('Release Date', releaseDate, true);
-      embed.addField('Vote Average', tmdbData.vote_average, true);
-      embed.setURL(`https://www.themoviedb.org/movie/${tmdbData.id}`);
-    } else {
-      const firstAirDate = tmdbData.first_air_date ? new Date(tmdbData.first_air_date).toLocaleDateString() : 'Unknown';
-      embed.addField('First Air Date', firstAirDate, true);
-      embed.addField('Vote Average', tmdbData.vote_average, true);
-      embed.setURL(`https://www.themoviedb.org/tv/${tmdbData.id}`);
-    }
-
-    if (tmdbData.poster_path) {
-      const posterUrl = `https://image.tmdb.org/t/p/w500${tmdbData.poster_path}`;
-      embed.setImage(posterUrl);
-    }
+  if (data.results.length > 0) {
+    const result = data.results[0];
+    const tmdbData = {
+      rating: result.vote_average,
+      year: result.release_date ? result.release_date.slice(0, 4) : 'N/A',
+      genres: result.genre_ids,
+      poster: `https://image.tmdb.org/t/p/w200${result.poster_path}`,
+    };
+    return tmdbData;
+  } else {
+    return null;
   }
-
-  return embed;
 }
 
-// Function to check if an item matches a filter
-function matchesFilter(item, filter, genre) {
-  // Check if the filter matches the title or genre of the item
-  return item.title.toLowerCase().includes(filter) || genre.includes(filter);
+async function searchTMDBByGenre(genre) {
+  const encodedGenre = encodeURIComponent(genre);
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&with_genres=${encodedGenre}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (data.results.length > 0) {
+    const results = data.results.slice(0, 5);
+    const tmdbData = results.map((result) => ({
+      title: result.title,
+      rating: result.vote_average,
+      year: result.release_date ? result.release_date.slice(0, 4) : 'N/A',
+    }));
+    return tmdbData;
+  } else {
+    return [];
+  }
 }
 
 client.login(discordToken);
